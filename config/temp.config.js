5ad3aa88283754e55d54c5f4be2bf54c0e036bc5
wb-mxl231031 <wb-mxl231031@cainiao.com>
2018-05-11 18:00:30 +0800

    add

--- config/temp.config.js
+++ config/temp.config.js
@@ -1,3 +1,30 @@
 const glob = require('glob');
+const upath = require('upath');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-console.log(glob);
+const APP_ROOT = process.cwd();
+let openPage = {};
+
+const getHtmlConfig = () => {
+  const foundScripts = glob.sync(`temp/*/*.js`, {});
+  const arr = [];
+
+  foundScripts.forEach(fullpath => {
+    // fullpath = upath.normalize(fullpath);
+    let chunk = fullpath.replace(/temp\//, '').replace(/^(.*)\.js$/, '$1');
+    let filename = path.join(APP_ROOT, fullpath.replace(/temp\//, 'dist/').replace(/\.js/, '.html'));
+    openPage[chunk] = path.join(fullpath.replace(/temp\//, '/').replace(/\.js/, '.html'));
+    arr.push(
+      new HtmlWebpackPlugin({
+        template: path.resolve(APP_ROOT, 'templates/tpl.html'),
+        chunks: [chunk],
+        inject: 'body',
+        filename: filename
+      })
+    );
+    console.log(fullpath);
+  });
+}
+
+getHtmlConfig();
